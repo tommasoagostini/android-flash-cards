4f61fb9d63fdc418be7174904eb563880146c982
tamsler <tamsler@cc5f21a0-4b53-2e07-d848-569cdd0bae5b>
2011-11-24 20:18:35 +0000

    Create tag for blog entry

---
+++ tags/blog-20111123/src/org/thomasamsler/android/flashcards/AppConstants.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2011 Thomas Amsler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+package org.thomasamsler.android.flashcards;
+
+public interface AppConstants {
+	
+	public static final String SELECTED_LIST_ITEM_KEY = "words";
+	public static final String LOG_TAG = "Flash Cards";
+	public static final String FILE_NAMES_KEY = "files";
+	
+	public static final int NORMAL_TEXT_SIZE = 60;
+	public static final int LARGE_TEXT_SIZE = 80;
+	
+	public static final String _OF_ = " of ";
+	
+	public static final String WORD_DELIMITER_TOKEN = ":";
+	
+
+}
